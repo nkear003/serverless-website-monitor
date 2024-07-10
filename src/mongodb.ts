a67411413b15697d67b53d18c30c6c15eaaf51c0
Nicholas Kearney <nkearney757@gmail.com>
2024-07-10 11:14:08 +0200

    Install mongodb and create connection file

---
+++ src/mongodb.ts
@@ -0,0 +1,36 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let cachedClient: MongoClient | null = null;
+
+export async function connectToDatabase() {
+  if (cachedClient) {
+    return cachedClient;
+  }
+
+  if (!process.env?.MONGO_URL) {
+    throw new Error("Missing mongodb connection string in env variables");
+  }
+
+  const client = new MongoClient(process.env.MONGO_URL);
+
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    cachedClient = client;
+    return client;
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    throw err;
+  }
+}
+
+export async function closeDatabaseConnection() {
+  if (cachedClient) {
+    await cachedClient.close();
+    console.log("MongoDB connection closed");
+    cachedClient = null;
+  }
+}
