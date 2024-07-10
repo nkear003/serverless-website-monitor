634ad320c8c42044b4b7fb312e218f96af180439
Nicholas Kearney <nkearney757@gmail.com>
2024-07-10 13:21:26 +0200

    Add process env declarations

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,25 @@
 export type Payload = {
   message: string;
 };
+
+namespace NodeJS {
+  interface ProcessEnv {
+    SENDGRID_API_KEY: string;
+    TWILIO_ACCOUNT_SID: string;
+    TWILIO_AUTH_TOKEN: string;
+    TWILIO_PHONE_NUMBER: string;
+    TO_PHONE_NUMBER: string;
+    TO_EMAIL: string;
+    FROM_EMAIL: string;
+    GOOGLE_SHEET_ID: string;
+    GOOGLE_CLIENT_EMAIL: string;
+    GOOGLE_PRIVATE_KEY: string;
+    MONITOR_URL: string;
+    ETHEREAL_PASSWORD: string;
+    ETHEREAL_USER: string;
+    ETHEREAL_NAME: string;
+    MONGO_CONNECTION_STRING: string;
+    DB_NAME: string;
+    DB_COLLECTION_NAME: string;
+  }
+}
