3c2675a93715c65ee91a43678ee10ca9a34dfb5b
Nicholas Kearney <nkearney757@gmail.com>
2024-07-11 12:08:13 +0200

    Update env example and generation script

--- create-env-example.sh
+++ create-env-example.sh
@@ -7,7 +7,7 @@ ENV_EXAMPLE_FILE=".env.example"
 # Check if .env file exists
 if [ -f "$ENV_FILE" ]; then
   # Copy .env to env.example and remove values after '='
-  awk -F '=' '{ print $1 "=" }' "$ENV_FILE" > "$ENV_EXAMPLE_FILE"
+  awk -F '=' 'NF && !/^[\t ]*$/ { print $1 "=" }' "$ENV_FILE" > "$ENV_EXAMPLE_FILE"
   echo ".env.example has been created/updated."
 else
   echo ".env file does not exist."
